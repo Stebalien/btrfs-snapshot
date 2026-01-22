54d77f3ac0895041d5483b4ac753604c696944f7
Steven Allen <steven@stebalien.com>
2026-01-22 08:31:21 -0800

    nix: remove flake-parts

--- flake.nix
+++ flake.nix
@@ -3,87 +3,86 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs @ { flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } (
-    { moduleWithSystem, ... }:
-    {
-      systems = [
+  outputs = { nixpkgs, self, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs [
         "i686-linux"
         "x86_64-linux"
         "aarch64-linux"
         "armv7l-linux"
       ];
-      imports = [ flake-parts.flakeModules.easyOverlay ];
-      perSystem = { config, system, lib, pkgs, ...}:
-        let
-          btrfs-snapshot = (pkgs.resholve.mkDerivation {
-            pname = "btrfs-snapshot";
-            version = "1.0.0";
-            src = ./.;
-            strictDeps = true;
-            nativeBuildInputs = with pkgs; [ gnumake m4 ];
-            makeFlags = [
-              "DESTDIR=$(out)"
-              "PREFIX="
-              "SYSTEMD_UNIT_DIR=share/systemd"
-            ];
-            solutions = {
-              btrfs-snapshot = {
-                interpreter = "${pkgs.bash}/bin/bash";
-                scripts = [
-                  "lib/btrfs-snapshot-common.sh"
-                  "bin/btrfs-snapshot"
-                  "bin/btrfs-snapshot-cleanup"
-                ];
-                inputs = with pkgs; [ util-linux btrfs-progs coreutils findutils git ];
-                execer = [
-                  "cannot:${pkgs.util-linux}/bin/flock"
-                  "cannot:${pkgs.git}/bin/git"
-                ];
+    in
+      {
+        packages = eachSystem (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            btrfs-snapshot = (pkgs.resholve.mkDerivation {
+              pname = "btrfs-snapshot";
+              version = "1.0.0";
+              src = ./.;
+              strictDeps = true;
+              nativeBuildInputs = with pkgs; [ gnumake m4 ];
+              makeFlags = [
+                "DESTDIR=$(out)"
+                "PREFIX="
+                "SYSTEMD_UNIT_DIR=share/systemd"
+              ];
+              solutions = {
+                btrfs-snapshot = {
+                  interpreter = "${pkgs.bash}/bin/bash";
+                  scripts = [
+                    "lib/btrfs-snapshot-common.sh"
+                    "bin/btrfs-snapshot"
+                    "bin/btrfs-snapshot-cleanup"
+                  ];
+                  inputs = with pkgs; [ util-linux btrfs-progs coreutils findutils git ];
+                  execer = [
+                    "cannot:${pkgs.util-linux}/bin/flock"
+                    "cannot:${pkgs.git}/bin/git"
+                  ];
+                };
               };
-            };
-          }).overrideAttrs (old: {
-            preFixup = ''
+            }).overrideAttrs (old: {
+              preFixup = ''
               substituteInPlace $out/share/systemd/*/*.service \
                 --replace-quiet "/bin/" "$out/bin/"
             '' + old.preFixup;
-          });
-        in rec {
-          packages = {
-            inherit btrfs-snapshot;
-            default = packages.btrfs-snapshot;
-          };
-          overlayAttrs = {
-            inherit (config.packages) btrfs-snapshot;
-          };
+            });
+          in
+            {
+              inherit btrfs-snapshot;
+              default = btrfs-snapshot;
+            }
+        );
+
+        overlays.default = final: prev: {
+          inherit (self.packages.${final.system}) btrfs-snapshot;
         };
-      flake.nixosModules.default = moduleWithSystem (
-        perSystem@{pkgs, self', ... }:
-        nixos@{lib, config, ... }:
-        let
-          cfg = config.services.btrfs-snapshot;
-        in
-        {
-          options.services.btrfs-snapshot = {
-            install = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-              description = ''
+
+        nixosModules.default = { lib, config, pkgs, ... }:
+          let
+            cfg = config.services.btrfs-snapshot;
+          in
+            {
+              options.services.btrfs-snapshot = {
+                install = lib.mkOption {
+                  type = lib.types.bool;
+                  default = false;
+                  description = ''
                 Whether to install the systemd units for btrfs-snapshot.
 
                 The units must be manually enabled for each subvolume.
               '';
+                };
+                package = lib.mkPackageOption pkgs "btrfs-snapshot" {
+                  default = pkgs.btrfs-snapshot;
+                };
+              };
+              config = lib.mkIf cfg.install {
+                systemd.packages = [ cfg.package ];
+              };
             };
-            package = lib.mkPackageOption self'.packages "btrfs-snapshot" { };
-          };
-          config = lib.mkIf cfg.install {
-            systemd.packages = [ cfg.package ];
-          };
-        });
-    });
-  }
+      };
+}
